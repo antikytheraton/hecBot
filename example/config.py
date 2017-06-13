a448d8e7194ba2f7d2f3fb9a21a66f437c0bb0ae
antikytheraton <binauralvoice@gmail.com>
2017-06-13 16:59:53 -0500

    hecbot first commit

---
+++ example/config.py
@@ -0,0 +1,5 @@
+CONFIG = {
+    'FACEBOOK_TOKEN': 'EAAV7Og9VPQ4BAAQZCjXQPZATfoAZChMnhZBAAIdApOrWrok3MSOuXb1C8XywZCvl5ntfd0yebZCqX7EGZBBOOiGF7JbNNeXdUqiC1LGfmO5ZA39flE1oUb4Gtmh5GmwcqlaZAvjelbBiNzIFO4IAhwZCLZCIB7ZB4uEM8kcvoBaovWillQZDZD',
+    'VERIFY_TOKEN': 'verify_hecbot',
+    'SERVER_URL': 'https://hec-bot.herokuapp.com/'
+}
